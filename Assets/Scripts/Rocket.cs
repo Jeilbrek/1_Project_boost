cbcc905e5473e3ab0135451d71700e343e1200d7
Adrián Martinez <adrimrtnz@gmail.com>
2020-04-19 11:11:04 +0200

    Rocket made & basic thruster coded

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            print("Rotating to the right");
+        } 
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            print("Rotating to the left");
+        }
+    }
+}
